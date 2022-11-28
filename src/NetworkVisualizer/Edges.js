bce7c66873168d57391a7b45faab25a40d34b1f7
Aleksandr Matyushik <34347587+AleksanderIII@users.noreply.github.com>
2022-11-28 11:31:03 +0300

    Update Edges.js
    fix arrow coordinates providing issue

--- src/NetworkVisualizer/Edges.js
+++ src/NetworkVisualizer/Edges.js
@@ -4,15 +4,24 @@ function Edges({ width, height, edges }) {
   return (
     <Stage width={width} height={height}>
       <Layer>
-        {edges.map((edge, i) => (
-          <Arrow
+        {edges.map((edge, i) => {
+          const lastPointIndex = edge.points.length - 1;
+          const arrowStartPoint = edge.points[0];
+          const arrowEndPoint = edge.points[lastPointIndex];
+
+          return <Arrow
             key={i}
-            points={edge.points}
+            points={[
+              arrowStartPoint.x,
+              arrowStartPoint.y,
+              arrowEndPoint.x,
+              arrowEndPoint.y
+            ]}
             strokeWidth={1}
             stroke={"black"}
             fill={"black"}
           />
-        ))}
+        })}
       </Layer>
     </Stage>
   );
